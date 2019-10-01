a459c1817baaab4a46255c3be337b448a71757e8
Bartłomiej Karczmarczyk <bartek.karczmarczyk@gmail.com>
2019-10-01 09:41:53 +0200

    Added /user and /post routes to App

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
 import Top from './Top';
 import New from './New';
+import User from './User';
+import Post from './Post';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Top} />
         <Route exact path="/new" component={New} />
+        <Route path="/user" component={User} />
+        <Route path="/post" component={Post} />
         <Route render={() => <h1>404</h1>} />
       </Switch>
     </Router>
