adedd5e1862fa580bbd323542cf5e9e1758c4bd9
Bartłomiej Karczmarczyk <bartek.karczmarczyk@gmail.com>
2019-10-02 18:34:09 +0200

    Updates

---
+++ precache-manifest.ceaae63ef7cf3f5957415b2c1ecfaba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca83a7c69b461aa7b292856d265e51b",
+    "url": "/brikp/hackerrank-api/index.html"
+  },
+  {
+    "revision": "c8d924f47e334fb14f07",
+    "url": "/brikp/hackerrank-api/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6971b1edbd346cd41d99",
+    "url": "/brikp/hackerrank-api/static/js/2.ea6ddb06.chunk.js"
+  },
+  {
+    "revision": "c8d924f47e334fb14f07",
+    "url": "/brikp/hackerrank-api/static/js/main.a1a499c5.chunk.js"
+  },
+  {
+    "revision": "162ebbcc5d4af981f876",
+    "url": "/brikp/hackerrank-api/static/js/runtime-main.b28ba49b.js"
+  }
+]);
